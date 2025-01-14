f3e3c4088a3f11c69a93b63206e42c4bc4b724c2
liulma <linda.ulma@skillio.ai>
2025-01-14 14:47:51 +0200

    Added filtering

--- exercise1.py
+++ exercise1.py
@@ -3,8 +3,14 @@ import pandas as pd
 # Create dataframe
 data = {
     'Name': ['Teresa', 'Maria', 'John', 'Peter', 'Tom'],
-    'Age': [30, 48, 22, 33, 25],
+    'Age': [30, 48, 22, 33, 24],
     'Score': [5, 8, 4, 9, 7]
 }
 
 df = pd.DataFrame(data)
+
+df_indexed = df.set_index('Name')
+
+filtered_df = df_indexed[df_indexed['Age'] > 25]
+
+print(filtered_df)
