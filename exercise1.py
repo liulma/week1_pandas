3bb1b43abe9bfc78ba6ac889019effccd2526e1f
liulma <linda.ulma@skillio.ai>
2025-01-14 14:56:38 +0200

    Converted datatype of column Score

--- exercise1.py
+++ exercise1.py
@@ -1,16 +1,20 @@
 import pandas as pd
 
-# Create dataframe
+# Exercise 1.1: Create dataframe
 data = {
     'Name': ['Teresa', 'Maria', 'John', 'Peter', 'Tom'],
-    'Age': [30, 48, 22, 33, 24],
+    'Age': [30, 48, 30, 33, 24],
     'Score': [5, 8, 4, 9, 7]
 }
 
 df = pd.DataFrame(data)
 
+# Exercise 1.2: Set the 'Name' column as the index and slice the DataFrame to display information about individuals over the age of 25
 df_indexed = df.set_index('Name')
 
 filtered_df = df_indexed[df_indexed['Age'] > 25]
 
 print(filtered_df)
+
+# Exercise 1.3: Convert data type of 'Score' column to float
+df['Score'] = df['Score'].astype(float)
