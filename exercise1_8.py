e5d3925f1635dba96d255b0fa0d7f0389ae7f56d
liulma <linda.ulma@skillio.ai>
2025-01-14 16:42:40 +0200

    Added value column

--- exercise1_8.py
+++ exercise1_8.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 #Exercise 1.8: Dataframe with datetime
 data = {
-    'Date': ['1994-02-23', '1990-01-24', '2023-09-23', '2015-03-07', '2022-05-27']
+    'Date': ['1994-02-23', '1990-01-24', '2023-09-23', '2015-03-07', '2022-05-27'],
+    'Value': [1, 2, 4, 8, 9]
 }
 
 df = pd.DataFrame(data)
