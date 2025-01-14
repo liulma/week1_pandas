c871baef0fc9090c1bbd3560e027cee44b01b83d
liulma <linda.ulma@skillio.ai>
2025-01-14 14:40:14 +0200

    Created dataframe

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Create dataframe
+data = {
+    'Name': ['Teresa', 'Maria', 'John', 'Peter', 'Tom'],
+    'Age': [30, 48, 22, 33, 25],
+    'Score': [5, 8, 4, 9, 7]
+}
+
+df = pd.DataFrame(data)
