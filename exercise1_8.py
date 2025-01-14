547c0a51717c572daa84e828c4e243cb349bcde8
liulma <linda.ulma@skillio.ai>
2025-01-14 16:34:31 +0200

    Final changes

---
+++ exercise1_8.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+#Exercise 1.8: Dataframe with datetime
+data = {
+    'Date': ['1994-02-23', '1990-01-24', '2023-09-23', '2015-03-07', '2022-05-27']
+}
+
+df = pd.DataFrame(data)
+
+df['Date'] = pd.to_datetime(df['Date'])
+
+print("Datetime:")
+print(df)
