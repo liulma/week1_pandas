5671fee6652eb21857dca5d4131d2ac8105b375b
liulma <linda.ulma@skillio.ai>
2025-01-15 09:59:06 +0200

    New dataset with average fare per survived

---
+++ exercise2_6.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Exercise 2.4: New dataset which displays the average fare per survived
+file_path = 'titanic.csv'
+
+df = pd.read_csv(file_path)
+
+grouped_df = df.groupby('Survived')['Fare'].mean().reset_index()
+
+print(grouped_df)
